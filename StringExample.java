8922b0490789a074b99ff9b3dd5e31c6dea8b3a8
chandanasiripurapu <98093919+chandanasiripurapu@users.noreply.github.com>
2022-02-07 19:16:49 +0530

    Add files via upload

---
+++ StringExample.java
@@ -0,0 +1,16 @@
+package pratice;
+
+public class StringExample {
+public static void main(String[] args) {
+		
+		String s = new String("Hello");
+		System.out.println("String "+s);
+		
+		s=s+"chandana";
+		
+		System.out.println("String "+s);
+		
+		//when you change value of s it will create new object
+	}
+
+}
