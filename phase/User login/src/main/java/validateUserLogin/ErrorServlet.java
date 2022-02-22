ee8bfaaa09bde5e84e38ad5e39d5c4f2ff592139
chandanasiripurapu <98093919+chandanasiripurapu@users.noreply.github.com>
2022-02-22 12:48:55 +0530

    Add files via upload

---
+++ phase/User login/src/main/java/validateUserLogin/ErrorServlet.java
@@ -0,0 +1,32 @@
+package validateUserLogin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/error")
+public class ErrorServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		PrintWriter out=response.getWriter();
+		out.print("Please enter valid Username and Password");
+		
+		out.print("<br> <a href=\"index.html\"><button type=\"button\">Login</button></a>");
+		
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
